736202510c1c587de23b06d9da12a3bbb4418d52
Sebastian Arrieta <sarrietav@unicartagena.edu.co>
2020-09-30 23:33:11 -0500

    Styled the emails and added an status message after sending

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,19 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener("DOMContentLoaded", function () {
   // Use buttons to toggle between views
-  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
-  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document
+    .querySelector("#inbox")
+    .addEventListener("click", () => load_mailbox("inbox"));
+  document
+    .querySelector("#sent")
+    .addEventListener("click", () => load_mailbox("sent"));
+  document
+    .querySelector("#archived")
+    .addEventListener("click", () => load_mailbox("archive"));
+  document.querySelector("#compose").addEventListener("click", compose_email);
 
   // Add event listener to the form
-  document.querySelector("#compose-form").addEventListener("submit", send_email);
+  document
+    .querySelector("#compose-form")
+    .addEventListener("submit", send_email);
 
   // By default, load the inbox
-  load_mailbox('inbox');
+  load_mailbox("inbox");
 });
 
-// TODO: Add a success or error message.
 function send_email(event) {
   // Modifies the default beheavor so it doesn't reload the page after submitting.
   event.preventDefault();
@@ -29,41 +35,26 @@ function send_email(event) {
     body: JSON.stringify({
       recipients: recipients,
       subject: subject,
-      body: body
-    })
+      body: body,
+    }),
   })
     // Take the return data and parse it in JSON format.
-    .then(response => response.json())
-    .then(result => {
-      load_mailbox("sent");
+    .then((response) => response.json())
+    .then((result) => {
+      load_mailbox("sent", result);
     })
-    .catch(error => alert(error));
-}
-
-function make_alert(message) {
-  const element = document.createElement('div');
-  element.innerHTML = content;
-  element.classList.add("alert");
-  if (message.has("message")) {
-    element.classList.add("alert-success");
-  }
-  else if (message.has("error")) {
-    element.classList.add("alert-danger");
-  }
-
-  document.querySelector("#message-div").appendChild(element);
+    .catch((error) => console.log(error));
 }
 
 function compose_email() {
-
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector("#emails-view").style.display = "none";
+  document.querySelector("#compose-view").style.display = "block";
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  document.querySelector("#compose-recipients").value = "";
+  document.querySelector("#compose-subject").value = "";
+  document.querySelector("#compose-body").value = "";
 }
 
 function load_mailbox(mailbox, message = "") {
@@ -72,29 +63,45 @@ function load_mailbox(mailbox, message = "") {
 
   // Print a message if any.
   if (message !== "") {
-    make_alert()
+    make_alert(message);
   }
 
   // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector("#emails-view").style.display = "block";
+  document.querySelector("#compose-view").style.display = "none";
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector("#emails-view").innerHTML = `<h3>${
+    mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
+    }</h3>`;
 
   fetch(`/emails/${mailbox}`)
-    .then(response => response.json())
-    .then(emails => {
+    .then((response) => response.json())
+    .then((emails) => {
       emails.forEach(show_email_item);
     })
-    .catch(error => console.error(error))
+    .catch((error) => console.error(error));
+}
+
+function make_alert(message) {
+  const element = document.createElement("div");
+  element.classList.add("alert");
+
+  if (message["message"]) {
+    element.classList.add("alert-success");
+    element.innerHTML = message["message"];
+  } else if (message["error"]) {
+    element.classList.add("alert-danger");
+    element.innerHTML = message["error"];
+  }
+
+  document.querySelector("#message-div").appendChild(element);
 }
 
 function show_email_item(item) {
   const parent_element = document.createElement("div");
 
   compose_div(item, parent_element);
-  parent_element.style.margin = "10px";
 
   // TODO: Add an event listener.
   // parent_element.addEventListener();
@@ -121,7 +128,12 @@ function compose_div(item, parent_element) {
   content.style.padding = "10px";
   parent_element.appendChild(content);
 
+  if (item["read"]) {
+    parent_element.style.backgroundColor = "grey";
+  }
+
   parent_element.style.margin = "5px";
   parent_element.style.borderStyle = "solid";
   parent_element.style.borderWidth = "3px";
+  parent_element.style.margin = "10px";
 }
